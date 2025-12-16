1474ad1be4fc88ce7d9313a2430b09abc76f9347
Sanya Modi <modisanya05@gmail.com>
2025-12-16 16:52:50 +0530

    [Sanya] : Added program to add two numbers

---
+++ gcr-codebase/program-prerequisite/AddTwoNumbers.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class AddTwoNumbers{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter First Number");
+        int num1 = sc.nextInt();
+        System.out.println("Enter Second Number");
+        int num2 = sc.nextInt();
+        int sum = num1 + num2;
+        System.out.println("Result : " + sum );
+
+}
+}
