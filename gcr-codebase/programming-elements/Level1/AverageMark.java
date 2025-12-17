e81c51f5b768ef9a4cf086d62aac0f044a99686a
Sanya Modi <modisanya05@gmail.com>
2025-12-17 11:57:53 +0530

    [Sanya] : Added program to find average mark

---
+++ gcr-codebase/programming-elements/Level1/AverageMark.java
@@ -0,0 +1,13 @@
+public class AverageMark{
+public static void main(String[] args){
+
+int maths = 94;
+int physics = 95;
+int chemistry = 96;
+
+float avg = (maths + physics + chemistry)/3;
+
+System.out.println("Sam's average mark in PCM is " + avg);
+
+}
+}
