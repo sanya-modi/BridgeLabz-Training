9def87d2530b4066c67dd7f1b3b6440728468859
Sanya Modi <modisanya05@gmail.com>
2025-12-17 16:31:13 +0530

    [Sanya] : modified program to calculate discounted fee

--- gcr-codebase/programming-elements/Level1/CourseFeeDiscount.java
+++ gcr-codebase/programming-elements/Level1/CourseFeeDiscount.java
@@ -1,9 +1,13 @@
 public class CourseFeeDiscount {
     public static void main(String[] args) {
+
         int fee = 125000;
         int discountPercent = 10;
+        //Calculate the discount
         int discount = (fee * discountPercent) / 100;
+        //Calculate final fee to pay after discount
         int finalFee = fee - discount;
+
         System.out.println("The discount amount is INR " + discount + " and final discounted fee is INR " + finalFee);
     }
 }
