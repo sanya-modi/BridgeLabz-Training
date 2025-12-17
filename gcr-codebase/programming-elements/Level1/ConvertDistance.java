91c4927953586a5e0f50c6627e263257daaafe98
Sanya Modi <modisanya05@gmail.com>
2025-12-17 16:28:39 +0530

    [Sanya] : modified program to convert distance

--- gcr-codebase/programming-elements/Level1/ConvertDistance.java
+++ gcr-codebase/programming-elements/Level1/ConvertDistance.java
@@ -1,10 +1,11 @@
 public class ConvertDistance{
-public static void main(String[] args){
+    public static void main(String[] args){
 
-float distanceInKm = 10.8f;
-float distanceInMiles = (distanceInKm * 1.6f);
+        float distanceInKm = 10.8f;
+        //Converting distance in Kilometer to distance in miles
+        float distanceInMiles = (distanceInKm * 1.6f);
 
-System.out.println("The distance " + distanceInKm +"km in miles is " + distanceInMiles); 
+        System.out.println("The distance " + distanceInKm +"km in miles is " + distanceInMiles); 
 
 }
 }
