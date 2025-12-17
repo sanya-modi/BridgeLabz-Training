e611679955cbad4cf9b6f2414e4e7ee247c80e0d
Sanya Modi <modisanya05@gmail.com>
2025-12-17 22:40:55 +0530

    [Sanya] : Added program to calculate total price

---
+++ gcr-codebase/programming-elements/Level1/TotalPurchasePrice.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class TotalPurchasePrice {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double unitPrice = sc.nextDouble();
+        int quantity = sc.nextInt();
+        //Calculate total price to buy all items
+        double totalPrice = unitPrice * quantity;
+
+        System.out.println("The total purchase price is INR " + totalPrice + " if the quantity " + quantity + " and unit price is INR " + unitPrice);
+    }
+}
+
