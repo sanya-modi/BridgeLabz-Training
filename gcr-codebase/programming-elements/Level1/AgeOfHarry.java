7f4bd3f137f1cebfd2fcb56781d2df399d7dca44
Sanya Modi <modisanya05@gmail.com>
2025-12-17 11:43:53 +0530

    [Sanya] : Added program to find age

---
+++ gcr-codebase/programming-elements/Level1/AgeOfHarry.java
@@ -0,0 +1,9 @@
+public class AgeOfHarry{
+public static void main(String[] args){
+
+int birthyear = 2000;
+int currentAge = 2024 - birthyear;
+
+System.out.println("Harry's age in 2024 is " + currentAge);
+}
+}
