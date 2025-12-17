4b07ab69a1a448c3e2a326e64ab61c2d73d36b49
Sanya Modi <modisanya05@gmail.com>
2025-12-17 16:06:59 +0530

    [Sanya] : modified program to calculate average mark

--- gcr-codebase/programming-elements/Level1/AverageMark.java
+++ gcr-codebase/programming-elements/Level1/AverageMark.java
@@ -1,13 +1,14 @@
 public class AverageMark{
-public static void main(String[] args){
+    public static void main(String[] args){
 
-int maths = 94;
-int physics = 95;
-int chemistry = 96;
+        int maths = 94;
+        int physics = 95;
+        int chemistry = 96;
+        //Calculating the average mark
+        float avg = (maths + physics + chemistry)/3;
 
-float avg = (maths + physics + chemistry)/3;
-
-System.out.println("Sam's average mark in PCM is " + avg);
+        //Printing the average mark
+        System.out.println("Sam's average mark in PCM is " + avg);
 
 }
 }
