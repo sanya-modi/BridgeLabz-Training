deaf274e60a18e4cb0acdaeeac80df0e85253520
Sanya Modi <modisanya05@gmail.com>
2025-12-17 16:47:29 +0530

    [Sanya] : modified program to calculate volume of earth

--- gcr-codebase/programming-elements/Level1/VolumeOfEarth.java
+++ gcr-codebase/programming-elements/Level1/VolumeOfEarth.java
@@ -1,8 +1,14 @@
+import java.util.*;
+
 public class VolumeOfEarth {
     public static void main(String[] args) {
         double radiusKm = 6378;
+        //Calculate volume of earth in kilometers
         double volumeKm = (4.0 / 3.0) * Math.PI * Math.pow(radiusKm, 3);
+        //Calculate volume of earth in miles 
         double volumeMiles = volumeKm * Math.pow(0.621371, 3);
+        
+        //Print the volume in both kilometers and miles
         System.out.println("The volume of earth in cubic kilometers is " + volumeKm + " and cubic miles is " + volumeMiles);
     }
 }
