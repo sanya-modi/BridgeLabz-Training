f94563db9d3af4339f930cc915c9b60334672967
Sanya Modi <modisanya05@gmail.com>
2025-12-17 14:14:09 +0530

    [Sanya] : Added program to convert distance units

---
+++ gcr-codebase/programming-elements/Level1/ConvertDistance.java
@@ -0,0 +1,10 @@
+public class ConvertDistance{
+public static void main(String[] args){
+
+float distanceInKm = 10.8f;
+float distanceInMiles = (distanceInKm * 1.6f);
+
+System.out.println("The distance " + distanceInKm +"km in miles is " + distanceInMiles); 
+
+}
+}
