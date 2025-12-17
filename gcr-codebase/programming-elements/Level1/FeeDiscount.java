ffee536f92114a6b9506fec9b368654d21c1cf62
Sanya Modi <modisanya05@gmail.com>
2025-12-17 16:33:08 +0530

    [Sanya] : modified program to calculate discount

--- gcr-codebase/programming-elements/Level1/FeeDiscount.java
+++ gcr-codebase/programming-elements/Level1/FeeDiscount.java
@@ -1,13 +1,16 @@
 import java.util.*;
 
 public class FeeDiscount{
-public static void main(String[] args){
-Scanner sc = new Scanner(System.in);
-int fee = sc.nextInt();
-int discountPercent = sc.nextInt();
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
 
-int discount = (fee * discountPercent) / 100;
-int finalFee = fee - discount;
+        int fee = sc.nextInt();
+        int discountPercent = sc.nextInt();
+        //Calculate the discount
+        int discount = (fee * discountPercent) / 100;
+        //Calculate final amount to pay
+        int finalFee = fee - discount;
+
         System.out.println("The discount amount is INR " + discount + " and final discounted fee is INR " + finalFee);
 
 }
