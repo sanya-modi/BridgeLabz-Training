1c30677de447d9cc66003a3feb8e7d28e3796208
Sanya Modi <modisanya05@gmail.com>
2025-12-17 16:35:46 +0530

    [Sanya] : modified program to convert distance unit

--- gcr-codebase/programming-elements/Level1/KmToMiles.java
+++ gcr-codebase/programming-elements/Level1/KmToMiles.java
@@ -3,8 +3,11 @@ import java.util.Scanner;
 public class KmToMiles {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+
         double km = input.nextDouble();
+        //Convert distance in kilometer to miles
         double miles = km / 1.6;
+
         System.out.println("The total miles is " + miles + " mile for the given " + km + " km");
     }
 }
