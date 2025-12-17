ea37023ad5c2b69d3f3bda375ca0e4a0804bbd45
Sanya Modi <modisanya05@gmail.com>
2025-12-17 15:29:53 +0530

    [Sanya] : modified program to convert height

--- gcr-codebase/programming-elements/Level1/HeightConverter.java
+++ gcr-codebase/programming-elements/Level1/HeightConverter.java
@@ -2,11 +2,26 @@ import java.util.Scanner;
 
 public class HeightConverter {
     public static void main(String[] args) {
+
+        // Create Scanner object to take input
         Scanner sc = new Scanner(System.in);
+
+        // Read height in centimeters
         double heightCm = sc.nextDouble();
+
+        // Convert centimeters to inches
         double totalInches = heightCm / 2.54;
+
+        // Calculate feet from inches
         int feet = (int) (totalInches / 12);
+
+        // Calculate remaining inches
         double inches = totalInches % 12;
-        System.out.println("Your Height in cm is " + heightCm + " while in feet is " + feet + " and inches is " + inches);
+
+        // Display the result
+        System.out.println("Your Height in cm is " + heightCm +
+                " while in feet is " + feet +
+                " and inches is " + inches);
     }
 }
+
