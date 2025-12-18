e1099d8cbc340798b7b5dd91746470bf9722551c
Sanya Modi <modisanya05@gmail.com>
2025-12-18 16:05:19 +0530

    [Sanya] : Add program to find sum of natural numbers using for loop

--- gcr-codebase/control-flow/Level1/SumOfNaturalNumbers.java
+++ gcr-codebase/control-flow/Level1/SumOfNaturalNumbers.java
@@ -4,19 +4,14 @@ public class SumOfNaturalNumbers{
     public static void main(String[] args){
     Scanner sc = new Scanner(System.in);
 
-        //Get an input and check if it is an natural number
+        //Get an integer input 
         int n = sc.nextInt();
-        // If a number is negative or 0, it is not a natural number
-        if(n<=0){
-        System.out.println("The number " + n + " is not a natural number");
-        return;
-        }
-
         //Calculate sum of n natural numbers using loop
         long sum = 0;
         for(int i=1; i<=n; i++){
         sum += i;
         }
+
         //Display the sum 
         System.out.println("The sum of " + n + " natural numbers is " + sum);
         
