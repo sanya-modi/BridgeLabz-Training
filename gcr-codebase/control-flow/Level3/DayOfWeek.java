e45e5119554e7639e01bc8f5db3c72f887e82ee7
Sanya Modi <modisanya05@gmail.com>
2025-12-19 13:09:58 +0530

    [Sanya] : Add a program to find day of week using date

---
+++ gcr-codebase/control-flow/Level3/DayOfWeek.java
@@ -0,0 +1,19 @@
+public class DayOfWeek{
+    public static void main(String[] args){
+
+        //Take date as input
+        int d = Integer.parseInt(args[0]); //Date
+        int m = Integer.parseInt(args[1]); //Month
+        int y = Integer.parseInt(args[2]); //Year
+
+        //Calculate the day of the week
+        double y0 = y-(14-m)/12;
+        double x = y0 + y0/100 + y0/400;
+        double m0 = m+12*((14-m)/12)-2;
+        double d0 =(d+x+31*m0/12)%7;
+
+        //Print day of the week
+        System.out.println("Day of the week : " + (int)d0);
+        
+}
+}
