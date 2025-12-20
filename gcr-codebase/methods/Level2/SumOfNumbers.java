ecf2b572cd601292d285af4a2bc49175281efb03
Sanya Modi <modisanya05@gmail.com>
2025-12-20 15:05:59 +0530

    [Sanya] : Add a program to find the sum of n natural numbers

---
+++ gcr-codebase/methods/Level2/SumOfNumbers.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class SumOfNumbers{
+
+    //sum of n natural numbers using recursion
+    public static long sumUsingRecursion(int n){
+       //Base Condition
+        if(n==1){
+        return 1;
+        }
+    return n + sumUsingRecursion(n - 1);
+    }
+
+    //the sum of n natural numbers using formula
+    public static long sumUsingFormula(int n){
+        long sum = (n*(n+1))/2;
+    return sum;
+    }
+
+    public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+
+        //Take the user input number
+        int n = sc.nextInt();
+        //check whether it's a Natural number
+        if(n>0){
+        System.out.println("Sum of n natural numbers using recursion : " + sumUsingRecursion(n));
+        System.out.println("Sum of n natural numbers using formula : " + sumUsingFormula(n));
+        System.out.println("The result from both computations is correct. : " + (sumUsingRecursion(n)==sumUsingFormula(n)));
+        }
+    }
+}
