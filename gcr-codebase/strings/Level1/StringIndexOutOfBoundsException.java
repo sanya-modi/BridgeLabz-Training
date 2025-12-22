e9e73f48642736686776da11d65553e344eefe6f
Sanya Modi <modisanya05@gmail.com>
2025-12-22 15:18:26 +0530

    [Sanya] : Add a program to demonstrate string out of bound exception

---
+++ gcr-codebase/strings/Level1/StringIndexOutOfBoundsException.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class StringIndexOutOfBoundsException{
+ 
+    //Method to demonstrate NullPointerException
+    public static char checkException(String text){
+        return text.charAt(text.length());
+    }
+
+    //
+    public static void handleException(String text){
+    try{
+    System.out.println(text.charAt(text.length()));
+    }
+    catch(Exception e){ 
+    System.out.println(" Exception occured : StringIndexOutOfBoundsException \n" + e.getMessage());
+    }
+    }
+    
+    public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+  
+    //Get input by user
+    String text = "Sanya";    
+  
+    try{
+    checkException(text);
+    }
+    catch(Exception e){ 
+    handleException(text);
+    }
+    }
+}
