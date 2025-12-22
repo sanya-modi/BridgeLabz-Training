5f0b458777434c3bcdfacfadb3b8ffe5d7869f3c
Sanya Modi <modisanya05@gmail.com>
2025-12-22 15:30:31 +0530

    [Sanya] : Add a program to demonstrate illegal argumentexception

---
+++ gcr-codebase/strings/Level1/IllegalArgumentException.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class IllegalArgumentException{
+ 
+    //Method to demonstrate IllegalArgumentException
+    public static String checkException(String text){
+        return text.substring(4, 2);
+    }
+
+    //
+    public static void handleException(String text){
+    try{
+    System.out.println(text.substring(4, 2));
+    }
+    catch(Exception e){ 
+    System.out.println(" Exception occured : IllegalArgumentException \n" + e.getMessage());
+    }
+    }
+    
+    public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+  
+    //Get input by user
+    String text = "Sanya";    
+  
+    try{
+    checkException(text);
+    }
+    catch(Exception e){ 
+    handleException(text);
+    }
+    }
+}
