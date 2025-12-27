033e4325b7076c9ace904d8fc488ca987da8678f
Sanya Modi <modisanya05@gmail.com>
2025-12-27 12:35:06 +0530

    [Sanya] : Added a program to remove duplicate element from sorted array

---
+++ leetcode-codebase/arrays/RemoveDuplicate.java
@@ -0,0 +1,14 @@
+//Remove Duplicates from sorted array
+
+class RemoveDuplicate{
+    public int removeDuplicates(int[] nums) {
+        int prev = 0;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[prev] != nums[i]) {
+                prev++;
+                nums[prev] = nums[i];
+            }
+        }
+        return prev + 1;
+    }
+}
