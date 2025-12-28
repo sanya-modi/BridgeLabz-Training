b72535f9da201c5063093b2726bc51818a4ba81a
Sanya Modi <modisanya05@gmail.com>
2025-12-29 01:23:03 +0530

    [Sanya] : Add a program to calculate length of last word in a string

---
+++ leetcode-codebase/string/WordLength.java
@@ -0,0 +1,10 @@
+
+class WordLength {
+    public int lengthOfLastWord(String s) {
+        String arr[] = s.split(" ");
+
+        String lastWord = arr[arr.length - 1];
+        
+        return lastWord.length();
+    }
+}
