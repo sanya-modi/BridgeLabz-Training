f0cb3909d88624e840b9cbc6b900eaf971476f44
Sanya Modi <modisanya05@gmail.com>
2025-12-28 12:54:49 +0530

    [Sanya] : Added a program of book by creating, class and contructor

--- java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/constructor/level1/Book.java
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/constructor/level1/Book.java
@@ -1,35 +1,31 @@
 package com.constructor.level1;
 
 public class Book {
+	//attributes
 	String title;
 	String author;
 	double price;
 	
+	//Default constructor
 	Book(){
 		this.title = "Sample Book";
 		this.author = "XYZ";
 		this.price = 100.0;
 	}
 	
+	//Parameterized constructor
 	Book(String title, String author, double price){
 		this.author = author;
 		this.title = title;
 		this.price = price;
 	}
 	
+	//Display the properties 
 	public void display() {
 	   System.out.println("title: " + title);
 	   System.out.println("author: " + author);
 	   System.out.println("price: " + price);
 	   System.out.println();
-	}
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Book b1 = new Book();
-		b1.display();
-		Book b2 = new Book("History", "ABC", 700);
-		b2.display();
 	}
 
 }
