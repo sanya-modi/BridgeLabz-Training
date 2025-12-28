7c459a3afd76669fddf9fd4109ce2289b34f77cf
Sanya Modi <modisanya05@gmail.com>
2025-12-28 12:55:18 +0530

    [Sanya] : Added a program to create and initialize object of book class

--- java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/constructor/level1/BookMain.java
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/constructor/level1/BookMain.java
@@ -4,8 +4,10 @@ public class BookMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//Initialize object using default constructor
 		Book book1 = new Book();
 		book1.display();
+		//Initialize object using parameterizes constructor
 		Book book2 = new Book("History", "ABC", 700);
 		book2.display();
 	}
