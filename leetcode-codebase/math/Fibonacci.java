bde5b6bd106fb9fa6acbd15ca143b820ba210907
Sanya Modi <modisanya05@gmail.com>
2025-12-31 01:43:38 +0530

    [Sanya] : Add a program to find fibonacci number

---
+++ leetcode-codebase/math/Fibonacci.java
@@ -0,0 +1,19 @@
+//Fibonacci Number
+/*
+The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, such that each number is the sum of the two preceding ones, starting from 0 and 1. That is,
+
+F(0) = 0, F(1) = 1
+F(n) = F(n - 1) + F(n - 2), for n > 1.
+Given n, calculate F(n).
+*/
+
+class Fibonacci {
+    public int fib(int n) {
+        if(n==0 || n==1){
+            return n;
+        }
+        int nm1 = fib(n-1);
+        int nm2 = fib(n-2);
+        return nm1+nm2;
+    }
+}
