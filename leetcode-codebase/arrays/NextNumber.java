f72a35ea19cce11ebbaf91aeaec76b94260220f1
Sanya Modi <modisanya05@gmail.com>
2025-12-31 01:52:21 +0530

    [Sanya] : Add a program to find next array of digits

---
+++ leetcode-codebase/arrays/NextNumber.java
@@ -0,0 +1,25 @@
+/*
+You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer. The digits are ordered from most significant to least significant in left-to-right order. The large integer does not contain any leading 0's.
+
+Increment the large integer by one and return the resulting array of digits.
+*/
+
+class NextNumber {
+    public int[] plusOne(int[] digits) {
+        int carry = 1;
+
+        for(int i=digits.length - 1; i>=0; i--){
+            if(digits[i] != 9){
+                digits[i]++;
+                return digits;
+            }
+            else{
+                digits[i] = 0;
+            }
+        }
+
+        int[] newDigits = new int[digits.length + 1];
+        newDigits[0] = 1;
+        return newDigits;
+    }
+}
