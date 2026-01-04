30832bbfaae1a7c0886f67e58574927c51e5b18b
Sanya Modi <modisanya05@gmail.com>
2026-01-04 18:19:53 +0530

    [Sanya] : Add a program for task scheduler

---
+++ java-dsa-practice/gcr-codebase/JavaDsaConcepts/src/task_scheduler/Node.java
@@ -0,0 +1,22 @@
+package task_scheduler;
+import java.util.*;
+
+public class Node {
+	
+	 int id;
+	    String name;
+	    double priority;
+	    Date date;
+
+	    Node next;
+
+	    public Node(int id, String name, double priority, Date date) {
+	        this.id = id;
+	        this.name = name;
+	        this.priority = priority;
+	        this.date = date;
+	        this.next = null;
+	    }
+	
+
+}
