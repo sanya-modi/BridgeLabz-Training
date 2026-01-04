593b50f0d35002ffea0b9a802ebb05ca7605d68a
Sanya Modi <modisanya05@gmail.com>
2026-01-04 12:00:02 +0530

    [Sanya] : Add a program for social media friend connection

---
+++ java-dsa-practice/gcr-codebase/JavaDsaConcepts/src/social_media_connections/FriendNode.java
@@ -0,0 +1,13 @@
+package social_media_connections;
+
+public class FriendNode {
+	
+	 int friendId;
+	    FriendNode next;
+
+	    public FriendNode(int friendId) {
+	        this.friendId = friendId;
+	        this.next = null;
+	    }
+	
+}
---
+++ java-dsa-practice/gcr-codebase/JavaDsaConcepts/src/social_media_connections/UserNode.java
@@ -0,0 +1,19 @@
+package social_media_connections;
+
+public class UserNode {
+	
+	 int userId;
+	    String name;
+	    int age;
+	    FriendNode friends; // Head of friend list
+	    UserNode next;
+
+	    public UserNode(int userId, String name, int age) {
+	        this.userId = userId;
+	        this.name = name;
+	        this.age = age;
+	        this.friends = null;
+	        this.next = null;
+	    }
+
+}
