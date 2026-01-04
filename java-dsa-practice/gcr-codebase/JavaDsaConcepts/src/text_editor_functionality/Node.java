c09b93f2fdc1ca2d7a1d9f45af52df302f1da2a6
Sanya Modi <modisanya05@gmail.com>
2026-01-04 16:46:51 +0530

    [Sanya] : Add a program for text editor undo/redo function

---
+++ java-dsa-practice/gcr-codebase/JavaDsaConcepts/src/text_editor_functionality/Node.java
@@ -0,0 +1,15 @@
+package text_editor_functionality;
+
+public class Node {
+	
+	 String text;   // text content at this state
+	    Node prev;     // previous state (undo)
+	    Node next;     // next state (redo)
+
+	    public Node(String text) {
+	        this.text = text;
+	        this.prev = null;
+	        this.next = null;
+	    }
+
+}
