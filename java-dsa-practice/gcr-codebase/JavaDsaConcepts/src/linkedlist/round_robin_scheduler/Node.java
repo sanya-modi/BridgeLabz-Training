82c61ace3045e60866d2fe9dbde68e93eea32509
Sanya Modi <modisanya05@gmail.com>
2026-01-04 19:07:05 +0530

    [Sanya] : Add a program for round robin scheduler

--- java-dsa-practice/gcr-codebase/JavaDsaConcepts/src/linkedlist/round_robin_scheduler/Node.java
+++ java-dsa-practice/gcr-codebase/JavaDsaConcepts/src/linkedlist/round_robin_scheduler/Node.java
@@ -1,5 +1,25 @@
-package round_robin_scheduler;
+package linkedlist.round_robin_scheduler;
 
 public class Node {
+	
+	 int processId;
+	    int burstTime;
+	    int remainingTime;
+	    int priority;
+
+	    int waitingTime;
+	    int turnaroundTime;
+
+	    Node next;
+
+	    public Node(int processId, int burstTime, int priority) {
+	        this.processId = processId;
+	        this.burstTime = burstTime;
+	        this.remainingTime = burstTime;
+	        this.priority = priority;
+	        this.waitingTime = 0;
+	        this.turnaroundTime = 0;
+	        this.next = null;
+	    }
 
 }
