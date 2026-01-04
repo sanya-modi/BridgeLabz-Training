b64794a9fb451155165dbbb501a4afb083ac4eee
Sanya Modi <modisanya05@gmail.com>
2026-01-04 19:10:24 +0530

    [Sanya] : Add a program for ticket reservation system

--- java-dsa-practice/gcr-codebase/JavaDsaConcepts/src/linkedlist/ticket_reservation/Node.java
+++ java-dsa-practice/gcr-codebase/JavaDsaConcepts/src/linkedlist/ticket_reservation/Node.java
@@ -1,5 +1,24 @@
-package ticket_reservation;
+package linkedlist.ticket_reservation;
+import java.util.Date;
 
 public class Node {
+	
+	int ticketId;
+    String customerName;
+    String movieName;
+    int seatNumber;
+    Date bookingTime;
+
+    Node next;
+
+    public Node(int ticketId, String customerName, String movieName,
+                int seatNumber, Date bookingTime) {
+        this.ticketId = ticketId;
+        this.customerName = customerName;
+        this.movieName = movieName;
+        this.seatNumber = seatNumber;
+        this.bookingTime = bookingTime;
+        this.next = null;
+    }
 
 }
