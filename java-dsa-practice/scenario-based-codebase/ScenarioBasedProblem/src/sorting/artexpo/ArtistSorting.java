571609865033f4af0b3c3c9186ca1bdb878318b5
Sanya Modi <modisanya05@gmail.com>
2026-01-18 04:41:14 +0530

    [Sanya] : Added a program to sort artist based on registration time by using insertion sort

--- java-dsa-practice/scenario-based-codebase/ScenarioBasedProblem/src/sorting/artexpo/ArtistSorting.java
+++ java-dsa-practice/scenario-based-codebase/ScenarioBasedProblem/src/sorting/artexpo/ArtistSorting.java
@@ -4,7 +4,7 @@ public class ArtistSorting {
 	
 	public static void insertionSort(Artist[] artist, int n) {
 		for(int i=0; i<n; i++) {
-			for(int j=0; j<i; j++) {
+			for(int j=0; j<n-1-i; j++) {
 				if(artist[j].time.compareTo(artist[j+1].time) > 0) {
 					Artist temp = artist[j];
 					artist[j] = artist[j+1];
