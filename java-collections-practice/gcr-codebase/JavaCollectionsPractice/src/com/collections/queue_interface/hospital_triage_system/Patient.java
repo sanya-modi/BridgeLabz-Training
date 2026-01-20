8ddf60a6e72d0fb1e8cd7e7cef7b6be20ff495c1
Sanya Modi <modisanya05@gmail.com>
2026-01-20 23:15:24 +0530

    [Sanya] : Added a program to simulate patients with severity

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/collections/queue_interface/hospital_triage_system/Patient.java
@@ -0,0 +1,12 @@
+package com.collections.queue_interface.hospital_triage_system;
+
+class Patient {
+
+    String name;
+    int severity;
+
+    public Patient(String name, int severity) {
+        this.name = name;
+        this.severity = severity;
+    }
+}
