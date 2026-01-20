4899cd0427bb294fb0af4fe6cdca2e3b46075faf
Sanya Modi <modisanya05@gmail.com>
2026-01-20 17:13:06 +0530

    [Sanya] : Added a program for ai-driven resume screening system using generics

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/generics/resumescreeningsystem/JobRole.java
@@ -0,0 +1,12 @@
+package com.generics.resumescreeningsystem;
+
+public abstract class JobRole {
+
+    String candidateName;
+
+    JobRole(String candidateName) {
+        this.candidateName = candidateName;
+    }
+
+    abstract void screen();
+}
