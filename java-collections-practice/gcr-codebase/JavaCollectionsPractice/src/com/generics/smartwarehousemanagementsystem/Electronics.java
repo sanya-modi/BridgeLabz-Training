f6c5e917f722bb0b3feeef8617a37c574d086626
Sanya Modi <modisanya05@gmail.com>
2026-01-20 17:08:06 +0530

    [Sanya] : Added a program to smart warehouse management system using generics

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/generics/smartwarehousemanagementsystem/Electronics.java
@@ -0,0 +1,13 @@
+package com.generics.smartwarehousemanagementsystem;
+
+public class Electronics extends WarehouseItem {
+
+    Electronics(String name) {
+        super(name);
+    }
+
+    @Override
+    void displayInfo() {
+        System.out.println("Electronics: " + name);
+    }
+}
---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/generics/smartwarehousemanagementsystem/Groceries.java
@@ -0,0 +1,13 @@
+package com.generics.smartwarehousemanagementsystem;
+
+public class Groceries extends WarehouseItem {
+
+    Groceries(String name) {
+        super(name);
+    }
+
+    @Override
+    void displayInfo() {
+        System.out.println("Grocery: " + name);
+    }
+}
