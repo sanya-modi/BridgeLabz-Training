b340f2d0958ff716992ef90b756c9cd0c11500a0
Sanya Modi <modisanya05@gmail.com>
2026-01-28 00:06:00 +0530

    [Sanya] : Added a program to generate csv report from database

---
+++ java-iostreams-practice/gcr-codebase/IOStreamsConcept/src/com/csvdata/dbtocsv/GenerateEmployeeCSV.java
@@ -0,0 +1,54 @@
+package com.csvdata.dbtocsv;
+
+import java.io.FileWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class GenerateEmployeeCSV {
+    public static void main(String[] args) {
+	// Verify MySQL Connector/J Is Loaded
+	/*
+	 * try { Class.forName("com.mysql.cj.jdbc.Driver");
+	 * System.out.println("MySQL Driver Loaded Successfully"); } catch
+	 * (ClassNotFoundException e) {
+	 * System.out.println("MySQL Driver NOT found in classpath");
+	 * e.printStackTrace(); return; }
+	 */
+
+	// sample database link
+    	String url = "jdbc:mysql://localhost:3306/student_db"
+    	           + "?useSSL=false"
+    	           + "&allowPublicKeyRetrieval=true"
+    	           + "&serverTimezone=UTC";
+
+    	String username = "root";
+    	String password = "Sanya@05";
+
+	// sample file path
+	String filePath = "C:/Users/modis/OneDrive/Pictures/Documents/students.csv";
+
+	try (Connection con = DriverManager.getConnection(url, username, password);
+		Statement stmt = con.createStatement();
+		ResultSet rs = stmt.executeQuery("SELECT id, name, department, salary FROM employees");
+		FileWriter writer = new FileWriter(filePath)) {
+
+	    // write header
+	    writer.write("Employee ID,Name,Department,Salary\n");
+
+	    while (rs.next()) {
+
+		// write each database record
+		writer.write(rs.getInt("id") + "," + rs.getString("name") + "," + rs.getString("department") + ","
+			+ rs.getInt("salary") + "\n");
+	    }
+
+	    System.out.println("CSV report generated successfully");
+
+	} catch (Exception e) {
+	    e.printStackTrace();
+	}
+
+    }
+}
