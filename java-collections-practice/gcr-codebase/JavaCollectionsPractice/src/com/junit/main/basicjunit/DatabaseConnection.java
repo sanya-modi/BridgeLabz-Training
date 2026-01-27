c96955ad21bfe126a19be4772b05ac13ad9c1929
Sanya Modi <modisanya05@gmail.com>
2026-01-27 12:17:56 +0530

    [Sanya] : Added a program for testing @BeforeEach and @AfterEach annotations

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/junit/main/basicjunit/DatabaseConnection.java
@@ -0,0 +1,20 @@
+package com.junit.main.basicjunit;
+
+public class DatabaseConnection {
+    private boolean isConnected;
+
+    public void connect() {
+        isConnected = true;
+        System.out.println("Database connected.");
+    }
+
+    public void disconnect() {
+        isConnected = false;
+        System.out.println("Database disconnected.");
+    }
+
+    public boolean isConnected() {
+        return isConnected;
+    }
+}
+
